c024f181f27c660398b65cf17c8ffbf7e438a126
erfankaboli <erfankaboli.ce@gmail.com>
2022-11-14 21:34:05 +0330

    OTAT-206 Add progress to the not accurate report alert

--- front/src/components/questionnaires/QuestionnairesNotCompleteAlert.tsx
+++ front/src/components/questionnaires/QuestionnairesNotCompleteAlert.tsx
@@ -5,22 +5,33 @@ import { Trans } from "react-i18next";
 import Button from "@mui/material/Button";
 import { Link, useLocation } from "react-router-dom";
 import { TId } from "../../types";
+import QANumberIndicator from "../shared/QANumberIndicator";
 
 type TQuestionnairesNotCompleteAlertProps = {
   subjectName?: string;
   subjectId?: TId;
+  progress?: number;
+  q?: number;
+  a?: number;
 };
 
 const QuestionnairesNotCompleteAlert = (
   props: TQuestionnairesNotCompleteAlertProps
 ) => {
-  const { subjectName, subjectId } = props;
+  const { subjectName, subjectId, progress, q, a } = props;
   const location = useLocation();
   return (
     <Alert
       severity="warning"
       variant="filled"
-      sx={{ backgroundColor: "#622301", color: "white", borderRadius: 2 }}
+      sx={{
+        backgroundColor: "#622301",
+        background: progress
+          ? `linear-gradient(135deg, #a53900 ${progress}%, #622301 ${progress}%)`
+          : undefined,
+        color: "white",
+        borderRadius: 2,
+      }}
       action={
         <Button
           color="inherit"
@@ -38,7 +49,19 @@ const QuestionnairesNotCompleteAlert = (
               : "questionnaires"
           }
         >
-          {subjectName} questionnaires
+          {subjectName} questionnaires{" "}
+          {q && a && (
+            <QANumberIndicator
+              q={q}
+              a={a}
+              sx={{
+                ml: 1,
+                pl: 1,
+                color: "white",
+                borderLeft: "1px dashed #ffffff52",
+              }}
+            />
+          )}
         </Button>
       }
     >
