274f21c6352afab5733d023da43a1979e1519b1c
sajjafari <sajjafari@duck.com>
2022-11-19 14:41:05 +0330

    Save and Load compare-object in session

---
+++ checkupassessmenttoolkit/assessment/fixture/dictionary.py
@@ -0,0 +1,13 @@
+
+class Dictionary(dict):
+ 
+  # __init__ function
+  def __init__(self):
+    self = dict()
+ 
+  # Function to add key:value
+  def add(self, key, value):
+    self[key] = value
+
+  def get(self, key):
+    return self[key]
