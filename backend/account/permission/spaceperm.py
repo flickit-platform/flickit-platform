a0b635184e990545ad997e666f16e2b31a258428
sajjafari <sajjafari@duck.com>
2022-11-29 13:08:15 +0330

    OTAT-232: compare service is ready now

--- backend/account/permission/spaceperm.py
+++ backend/account/permission/spaceperm.py
@@ -1,6 +1,7 @@
 from rest_framework.permissions import BasePermission
 from assessment.models import AssessmentResult, AssessmentProject
 
+ASSESSMENT_LIST_IDS_PARAM_NAME = 'assessment_list_ids'
 
 class IsSpaceMember(BasePermission):
     def has_permission(self, request, view):
@@ -25,6 +26,16 @@ class IsSpaceMember(BasePermission):
             assessment_project = AssessmentResult.objects.get(id = request.query_params.get('assessment_result_pk')).assessment_project
             return self.has_permision_for_assessment_result(assessment_project, current_user_space_list)
 
+        if request.data.get('assessment_list_ids') is not None:
+            assessment_list_ids = request.data.get(ASSESSMENT_LIST_IDS_PARAM_NAME)
+            are_assessment_ids_accessable = True
+            for assessment_id in assessment_list_ids:
+                assessment_project = AssessmentProject.objects.get(id = assessment_id)
+                if self.has_permision_for_assessment_result(assessment_project, current_user_space_list) != True:
+                    are_assessment_ids_accessable = False
+                    break
+            return are_assessment_ids_accessable
+
     def has_permision_for_assessment_result(self, assessment_project, current_user_space_list):
         assessment_list = []
         for space in current_user_space_list:
@@ -37,4 +48,5 @@ class IsSpaceMember(BasePermission):
         for space in current_user_space_list:
                 if str(space.id) == view.kwargs.get('space_pk'):
                     return True
+
     
