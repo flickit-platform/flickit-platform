3abb6ee7c8b66cb752bf5079ace513c8cd44bf06
erfankaboli <erfankaboli.ce@gmail.com>
2023-02-07 23:44:25 +0330

    OTAT-360 Add invitees and delete to expert group member

--- frontend/src/utils/setDocumentTitle.ts
+++ frontend/src/utils/setDocumentTitle.ts
@@ -1,7 +1,7 @@
 import { APP_LABEL } from "../config/constants";
 
 const setDocumentTitle = (title: string = "") => {
-  document.title = `${title}${title ? " - " : ""}${APP_LABEL}`;
+  document.title = `${title}${title ? " | " : ""}${APP_LABEL}`;
 };
 
 export default setDocumentTitle;
