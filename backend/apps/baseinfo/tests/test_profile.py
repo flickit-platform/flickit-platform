ee910705de282cc04735ef1815d68a18b3a71f71
sajjafari <sajjafari@duck.com>
2023-04-29 17:53:17 +0330

    fix quality_attribute_set problem

--- backend/apps/baseinfo/tests/test_profile.py
+++ backend/apps/baseinfo/tests/test_profile.py
@@ -4,7 +4,10 @@ from model_bakery import baker
 
 from assessment.models import AssessmentProfile, AssessmentProject
 from baseinfo.models.profilemodels import ExpertGroup
+from baseinfo.models.basemodels import AssessmentSubject
 from account.models import User
+from django.contrib.auth.models import Permission
+
 
 
 @pytest.mark.django_db
@@ -47,3 +50,27 @@ class Test_Delete_Profile:
 
         assert response.status_code == status.HTTP_403_FORBIDDEN
         assert response.data['message'] == 'You do not have permission to perform this action.'
+
+
+@pytest.mark.django_db
+class Test_Analyse_Profile:
+    def test_analyse_profile(self, api_client, authenticate, init_data):
+        authenticate(is_staff=True)
+        test_user = User.objects.get(email = 'test@test.com')
+        permission = Permission.objects.get(name='Manage Expert Groups')
+        test_user.user_permissions.add(permission)
+        test_user.save()
+        profile = baker.make(AssessmentProfile, title = "p1")
+        expert_group = baker.make(ExpertGroup)
+        expert_group.users.add(test_user)
+        profile.expert_group = expert_group
+        profile.save()
+        base_info = init_data()
+        response = api_client.get('/baseinfo/analyzeprofile/' + str(profile.id) + "/")
+        analyze_list = response.data
+        assert analyze_list[0]['metrics_number_by_level'][0]['metric_number'] == 2
+        assert analyze_list[0]['metrics_number_by_level'][1]['metric_number'] == 2
+        assert analyze_list[0]['metrics_number_by_level'][2]['metric_number'] == 3
+        assert analyze_list[0]['metrics_number_by_level'][3]['metric_number'] == 2
+        assert analyze_list[0]['metrics_number_by_level'][4]['metric_number'] == 2
+        assert response.status_code == status.HTTP_200_OK
