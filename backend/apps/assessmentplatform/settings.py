9704b050f133ab99df9fc24002c4744323d2ff85
sajjafari <sajjafari@duck.com>
2023-04-29 14:30:15 +0330

    Change some installation config

--- backend/apps/assessmentplatform/settings.py
+++ backend/apps/assessmentplatform/settings.py
@@ -69,7 +69,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST = os.environ.get('EMAIL_HOST'),
 EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER'),
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD'),
-EMAIL_PORT = 25
+EMAIL_PORT = os.environ.get('EMAIL_HOST_PORT'),
 DEFAULT_FROM_EMAIL =  os.environ.get('DEFAULT_FROM_EMAIL'),
 EMAIL_USE_TLS=True
 
