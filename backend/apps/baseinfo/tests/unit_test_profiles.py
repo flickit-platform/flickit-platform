7366b68816bac3f35ae82a17ddadbbff1b2acab1
sajjafari <sajjafari@duck.com>
2023-05-15 18:18:11 +0330

    resolve some conflict with main branch

--- backend/apps/baseinfo/tests/unit_test_profiles.py
+++ backend/apps/baseinfo/tests/unit_test_profiles.py
@@ -1,8 +1,7 @@
 import json
 
 from django.test import TestCase
-from baseinfo.services import profileservice
-from account.models import User, Space, UserAccess
+from account.models import User
 from baseinfo.models.profilemodels import ExpertGroup, AssessmentProfile, ProfileTag
 from model_bakery import baker
 
