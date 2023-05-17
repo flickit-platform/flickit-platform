89b3734706f3bbb05add326f9485a3dd20921e94
sajjafari <sajjafari@duck.com>
2023-05-17 18:57:56 +0330

    adjust calculation and report services to new data model

--- backend/apps/assessment/services/maturitylevel.py
+++ backend/apps/assessment/services/maturitylevel.py
@@ -1,4 +1,5 @@
 from baseinfo.models.metricmodels import MetricImpact, OptionValue
+from baseinfo.services import maturitylevelservices
 from assessment.fixture.dictionary import Dictionary
 from assessment.models import MetricValue
 
@@ -13,7 +14,7 @@ def calculate_maturity_level(result, quality_attribute):
                 if impact.quality_attribute.id == quality_attribute.id:
                     impacts.append(impact)
             if impacts:
-                sorted_impacts = sorted(impacts, key=lambda x: x.level, reverse=False)
+                sorted_impacts = sorted(impacts, key=lambda x: x.maturity_level.value, reverse=False)
                 metric_impact_attribute.add(metric_value, sorted_impacts)
 
         impact_metric_value_level_1 = Dictionary()
@@ -32,14 +33,14 @@ def calculate_maturity_level(result, quality_attribute):
                 # # for impact_op_value in impact_option_values:
                 # #     if impact_op_value.option is not None and 
                 answer_value = next((x for x in list(impact_option_values) if x.option.id == metric_value.answer.id), None)
-                if impact.level == 1 and metric_value.answer is not None:
+                if impact.maturity_level.value == 1 and metric_value.answer is not None:
                     if answer_value is None:   
                         effectivr_answer_value = OptionValue.objects.get(metric_impact = impact, value = 1)
                         impact_metric_value_level_1.add(impact, effectivr_answer_value)     
                     else:
                         impact_metric_value_level_1.add(impact, answer_value)
                         is_in_impact_level1 = True
-                elif impact.level == 2 and metric_value.answer is not None:
+                elif impact.maturity_level.value == 2 and metric_value.answer is not None:
                     if answer_value is None and is_in_impact_level1:   
                         effectivr_answer_value = OptionValue.objects.get(metric_impact = impact, value = 0)
                         impact_metric_value_level_2.add(impact, effectivr_answer_value) 
@@ -50,7 +51,7 @@ def calculate_maturity_level(result, quality_attribute):
                         impact_metric_value_level_2.add(impact, answer_value)
                         is_in_impact_level2 = True
                     
-                elif impact.level == 3 and metric_value.answer is not None:
+                elif impact.maturity_level.value == 3 and metric_value.answer is not None:
                     impact_metric_value_level_3.add(impact, answer_value)
                     if answer_value is None and (is_in_impact_level1 or is_in_impact_level2):   
                         effectivr_answer_value = OptionValue.objects.get(metric_impact = impact, value = 0)
@@ -60,7 +61,7 @@ def calculate_maturity_level(result, quality_attribute):
                         impact_metric_value_level_3.add(impact, effectivr_answer_value) 
                     else:
                         impact_metric_value_level_3.add(impact, answer_value)
-                elif impact.level == 4 and metric_value.answer is not None:
+                elif impact.maturity_level.value == 4 and metric_value.answer is not None:
                     impact_metric_value_level_4.add(impact, answer_value)
                     
 
@@ -82,22 +83,26 @@ def calculate_maturity_level(result, quality_attribute):
             for impact, option_value in impact_metric_value_level_dict.items():
                 sum_of_values += option_value.value
             if i == 1 and len(impact_metric_value_level_dict) != 0:
-                impacts = MetricImpact.objects.filter(level=1,quality_attribute=quality_attribute.id)
+                impact_maturity_level = maturitylevelservices.extract_maturity_level_by_value(profile=result.assessment_project.assessment_profile, value=1)
+                impacts = MetricImpact.objects.filter(maturity_level=impact_maturity_level, quality_attribute=quality_attribute.id)
                 score_level_1 = sum_of_values/len(impacts)
                 if score_level_1 >= 0.6:
                     maturity_level_value = 1
             if i == 2 and len(impact_metric_value_level_dict) != 0:
-                impacts = MetricImpact.objects.filter(level=2,quality_attribute=quality_attribute.id)
+                impact_maturity_level = maturitylevelservices.extract_maturity_level_by_value(profile=result.assessment_project.assessment_profile, value=2)
+                impacts = MetricImpact.objects.filter(maturity_level=impact_maturity_level, quality_attribute=quality_attribute.id)
                 score_level_2 = sum_of_values/len(impacts)
                 if score_level_1 >= 0.7 and score_level_2 >= 0.6:
                     maturity_level_value = 2
             if i == 3 and len(impact_metric_value_level_dict) != 0:
-                impacts = MetricImpact.objects.filter(level=3,quality_attribute=quality_attribute.id)
+                impact_maturity_level = maturitylevelservices.extract_maturity_level_by_value(profile=result.assessment_project.assessment_profile, value=3)
+                impacts = MetricImpact.objects.filter(maturity_level=impact_maturity_level, quality_attribute=quality_attribute.id)
                 score_level_3 = sum_of_values/len(impacts)
                 if score_level_1 >= 0.8 and score_level_2 >= 0.7 and score_level_3 >= 0.6:
                     maturity_level_value = 3
             if i == 4 and len(impact_metric_value_level_dict) != 0:
-                impacts = MetricImpact.objects.filter(level=4,quality_attribute=quality_attribute.id)
+                impact_maturity_level = maturitylevelservices.extract_maturity_level_by_value(profile=result.assessment_project.assessment_profile, value=4)
+                impacts = MetricImpact.objects.filter(maturity_level=impact_maturity_level, quality_attribute=quality_attribute.id)
                 score_level_4 = sum_of_values/len(impacts)
                 if score_level_1 >= 0.9 and score_level_2 >= 0.8 and score_level_3 >= 0.7 and score_level_4 >= 0.6:
                     maturity_level_value = 4
