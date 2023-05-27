587de9165933130aed7af2f3abe1ac7f372eaade
amirrezaflh1402 <amirrezaflh1402@gmail.com>
2023-05-27 11:47:06 +0330

    OTAT-474:radar chart bug fixed

--- frontend/src/utils/convertToSubjectChartData.ts
+++ frontend/src/utils/convertToSubjectChartData.ts
@@ -1,7 +1,7 @@
 const convertToSubjectChartData = (data: any[]) => {
   return data.map((item) => {
     return {
-      ml: item?.maturity_level_value,
+      ml: item?.maturity_level?.value,
       cl: 1,
       title: item?.quality_attribute?.title,
       id: item?.quality_attribute?.id,
