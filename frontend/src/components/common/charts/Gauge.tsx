dff0909a15607ab38685ca7cea22e80d1fe82e1a
amirrezaflh1402 <amirrezaflh1402@gmail.com>
2023-06-18 20:48:12 +0330

    OTAT-523:huskey ckeck

--- frontend/src/components/common/charts/Gauge.tsx
+++ frontend/src/components/common/charts/Gauge.tsx
@@ -27,7 +27,7 @@ const Gauge = (props: IGaugeProps) => {
   const colorPallet = getMaturityLevelColors(maturity_level_number);
   const colorCode = colorPallet[level_value - 1];
   const GaugeComponent = lazy(
-    () => import(`./GaugeComponent${maturity_level_number}.tsx`)
+    () => import(`./GaugeComponent${maturity_level_number}.tsx`) 
   );
   return (
     <Box p={1} position="relative" width="100%" {...rest}>
