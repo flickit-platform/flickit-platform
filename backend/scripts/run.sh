aaddbca2d27c86196d778b6dd59ba11acee2619b
sajjafari <sajjafari@duck.com>
2023-06-18 12:06:29 +0330

    fix celery connection

--- backend/scripts/run.sh
+++ backend/scripts/run.sh
@@ -10,3 +10,5 @@ uwsgi --http :8000 --workers 4 --master --enable-threads --module assessmentplat
  --buffer-size 32768 \
  --static-map /static=/vol/web/static \
  --static-map /media=/vol/web/media
+
+celery -A assessmentplatform worker -l info
