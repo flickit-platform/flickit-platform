6ae943022ddb7eedce4cce2fa6dde2527ab7b549
sajjafari <sajjafari@duck.com>
2023-06-19 14:48:27 +0330

    add ssl config for email in env

--- backend/apps/assessmentplatform/settings.py
+++ backend/apps/assessmentplatform/settings.py
@@ -71,7 +71,8 @@ EMAIL_HOST_USER = os.environ.get('EMAIL_HOST_USER')
 EMAIL_HOST_PASSWORD = os.environ.get('EMAIL_HOST_PASSWORD')
 EMAIL_PORT = os.environ.get('EMAIL_HOST_PORT')
 DEFAULT_FROM_EMAIL =  os.environ.get('DEFAULT_FROM_EMAIL')
-EMAIL_USE_TLS=True
+EMAIL_USE_TLS = os.environ.get('EMAIL_USE_TLS')  == 'True'
+EMAIL_USE_SSL = os.environ.get('EMAIL_USE_SSL') == 'True'
 
 DOMAIN = (os.environ.get('DOMAIN')) 
 SITE_NAME = ('Flickit') 
