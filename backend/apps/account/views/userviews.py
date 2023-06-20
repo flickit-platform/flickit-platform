5efa7ee8cc656b83d1bbda06cc664ad1ef0ebfa0
sajjafari <sajjafari@duck.com>
2023-06-20 15:34:47 +0330

    add request schema to user activation

--- backend/apps/account/views/userviews.py
+++ backend/apps/account/views/userviews.py
@@ -32,10 +32,10 @@ class UserActivationView(APIView):
     permission_classes = [AllowAny]
     def get (self, request, uid, token):
         protocol = 'https://' if request.is_secure() else 'http://'
-        # web_url = protocol + 'localhost:8000'
+        web_url = protocol + 'localhost:8000'
         print('protocol:' + protocol)
         print('before call')
-        post_url = "localhost:8000/authinfo/users/activation/?uid=(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$'"
+        post_url = web_url + "/authinfo/users/activation/?uid=(?P<uid>[\w-]+)/(?P<token>[\w-]+)/$'"
         post_data = {'uid': uid, 'token': token}
         result = requests.post(post_url, data = post_data)
         print('after call')
