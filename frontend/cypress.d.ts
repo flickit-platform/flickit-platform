6d7029608256b4c6e9451a5949ec2f2c2722520b
alialibalaei <38320841+alialibalaei@users.noreply.github.com>
2023-08-09 18:05:54 +0330

    Update cypress.d.ts

--- frontend/cypress.d.ts
+++ frontend/cypress.d.ts
@@ -5,7 +5,7 @@ import { mount } from "cypress/react";
 // Alternatively, can be defined in cypress/support/component.d.ts
 // with a <reference path="./component" /> at the top of your spec.
 declare global {
-  namespace Cypress {
+  namespace Cypress { 
     interface Chainable {
       mount: typeof mount;
     }
