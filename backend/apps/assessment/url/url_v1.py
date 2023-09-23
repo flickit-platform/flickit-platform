ea54f1e08bbc7597e42f272f8c9d1af956b146d0
alialibalaei <ali.alibalaii778@gmail.com>
2023-09-23 13:56:36 +0330

    Changed the response service  assessment colors list.

--- backend/apps/assessment/url/url_v1.py
+++ backend/apps/assessment/url/url_v1.py
@@ -1,5 +1,7 @@
 from django.urls import path
-from assessment.views import projectviews
+from assessment.views import projectviews, commonviews
+
 urlpatterns = [
     path("", projectviews.AssessmentProjectApi.as_view()),
+    path("colors/", commonviews.ColorApi.as_view()),
 ]
