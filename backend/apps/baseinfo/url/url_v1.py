eb8d75939cea30c16b0453a0b9e990ee612a2dcb
alialibalaei <ali.alibalaii778@gmail.com>
2023-10-14 10:55:45 +0330

    changed assessment kit urls.

--- backend/apps/baseinfo/url/url_v1.py
+++ backend/apps/baseinfo/url/url_v1.py
@@ -3,14 +3,14 @@ from django.urls import path
 from baseinfo.views import assessmentkitviews, commonviews
 
 urlpatterns = [
-        path("assessment-kits/<str:assessment_kit_id>/info/",assessmentkitviews.LoadAssessmentKitInfoEditableApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/stats/",assessmentkitviews.LoadAssessmentKitInfoStatisticalApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/",assessmentkitviews.EditAssessmentKitInfoApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/maturity-levels/",assessmentkitviews.LoadMaturityLevelApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/details/", assessmentkitviews.LoadAssessmentKitDetailsApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/details/subjects/<str:subject_id>/", commonviews.LoadAssessmentSubjectDetailsApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/details/attributes/<str:attribute_id>/", commonviews.LoadQualityAttributesDetailsApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/details/attributes/<str:attribute_id>/maturity-levels/<str:maturity_level_id>/", commonviews.LoadMaturityLevelsDetailsApi.as_view() ),
-        path("assessment-kits/<str:assessment_kit_id>/details/questionnaires/<str:questionnaire_id>/", commonviews.LoadQuestionnairesDetailsApi.as_view()),
-        path("assessment-kits/<str:assessment_kit_id>/details/questions/<str:question_id>/", commonviews.LoadQuestionDetailsApi.as_view()),
+        path("<str:assessment_kit_id>/info/",assessmentkitviews.LoadAssessmentKitInfoEditableApi.as_view()),
+        path("<str:assessment_kit_id>/stats/",assessmentkitviews.LoadAssessmentKitInfoStatisticalApi.as_view()),
+        path("<str:assessment_kit_id>/",assessmentkitviews.EditAssessmentKitInfoApi.as_view()),
+        path("<str:assessment_kit_id>/maturity-levels/",assessmentkitviews.LoadMaturityLevelApi.as_view()),
+        path("<str:assessment_kit_id>/details/", assessmentkitviews.LoadAssessmentKitDetailsApi.as_view()),
+        path("<str:assessment_kit_id>/details/subjects/<str:subject_id>/", commonviews.LoadAssessmentSubjectDetailsApi.as_view()),
+        path("<str:assessment_kit_id>/details/attributes/<str:attribute_id>/", commonviews.LoadQualityAttributesDetailsApi.as_view()),
+        path("<str:assessment_kit_id>/details/attributes/<str:attribute_id>/maturity-levels/<str:maturity_level_id>/", commonviews.LoadMaturityLevelsDetailsApi.as_view() ),
+        path("<str:assessment_kit_id>/details/questionnaires/<str:questionnaire_id>/", commonviews.LoadQuestionnairesDetailsApi.as_view()),
+        path("<str:assessment_kit_id>/details/questions/<str:question_id>/", commonviews.LoadQuestionDetailsApi.as_view()),
             ]
