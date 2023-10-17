47bddd161d9386e3a658d05cc75e9b4d95ffd925
alialibalaei <ali.alibalaii778@gmail.com>
2023-10-17 19:32:48 +0330

    Create Evidence delete service.

--- backend/apps/assessment/url/url_evidences.py
+++ backend/apps/assessment/url/url_evidences.py
@@ -2,4 +2,5 @@ from django.urls import path
 from assessment.views import evidenceviews
 urlpatterns = [
     path("", evidenceviews.EvidencesApi.as_view()),
+    path("<uuid:evidence_id>/", evidenceviews.EvidenceApi.as_view()),
 ]
