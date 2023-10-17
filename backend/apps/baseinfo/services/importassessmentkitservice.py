e3ad316f2b4ff8abf556575111eb890466690325
alialibalaei <ali.alibalaii778@gmail.com>
2023-10-17 19:30:24 +0330

    adding description in import assessment kit service.

--- backend/apps/baseinfo/services/importassessmentkitservice.py
+++ backend/apps/baseinfo/services/importassessmentkitservice.py
@@ -1,7 +1,7 @@
 import os
 
 from zipfile import ZipFile
-from django.utils.text import slugify 
+from django.utils.text import slugify
 from django.db import transaction
 
 from common.restutil import ActionResult
@@ -10,8 +10,9 @@ from baseinfo.models.questionmodels import Question, QuestionImpact, AnswerTempl
 from baseinfo.models.assessmentkitmodels import AssessmentKit, AssessmentKitDsl, MaturityLevel, LevelCompetence
 from baseinfo.services import assessmentkitservice, expertgroupservice
 
+
 def extract_dsl_contents(dsl_id):
-    dsl = AssessmentKitDsl.objects.get(id = dsl_id)
+    dsl = AssessmentKitDsl.objects.get(id=dsl_id)
     input_zip = ZipFile(dsl.dsl_file)
     all_content = ''
     for name in input_zip.namelist():
@@ -19,6 +20,7 @@ def extract_dsl_contents(dsl_id):
         trim_content = __trim_content(content)
         all_content = all_content + '\n' + trim_content
     return all_content
+
 
 def __trim_content(content):
     new_content = ''
@@ -28,13 +30,14 @@ def __trim_content(content):
             new_content = new_content + '\n' + line
     return new_content
 
+
 @transaction.atomic
 def import_assessment_kit(descriptive_assessment_kit, **kwargs):
     assessment_kit = __import_assessment_kit_base_info(kwargs)
     __import_maturity_levels(descriptive_assessment_kit, assessment_kit)
     level_models = descriptive_assessment_kit['levelModels']
     for level_model in level_models:
-        level_model_competence_dict = level_model['levelCompetence'] 
+        level_model_competence_dict = level_model['levelCompetence']
         if level_model_competence_dict is None:
             continue
         for m_l_title, competence_value in level_model_competence_dict.items():
@@ -49,6 +52,7 @@ def import_assessment_kit(descriptive_assessment_kit, **kwargs):
     __import_questions(descriptive_assessment_kit['questionModels'], assessment_kit)
     return assessment_kit
 
+
 def __import_maturity_levels(descriptive_assessment_kit, assessment_kit):
     level_models = descriptive_assessment_kit['levelModels']
     for level_model in level_models:
@@ -58,8 +62,10 @@ def __import_maturity_levels(descriptive_assessment_kit, assessment_kit):
         maturity_level.assessment_kit = assessment_kit
         maturity_level.save()
 
+
 def extract_maturity_level_by_title(title, assessment_kit):
-    return MaturityLevel.objects.get(title = title, assessment_kit = assessment_kit)
+    return MaturityLevel.objects.get(title=title, assessment_kit=assessment_kit)
+
 
 def extract_tags(tag_ids):
     tags = []
@@ -69,6 +75,7 @@ def extract_tags(tag_ids):
             if tag:
                 tags.append(tag)
     return tags
+
 
 @transaction.atomic
 def __import_assessment_kit_base_info(extra_info):
@@ -84,10 +91,11 @@ def __import_assessment_kit_base_info(extra_info):
     for tag in tags:
         assessment_kit.tags.add(tag)
     assessment_kit.save()
-    dsl = AssessmentKitDsl.objects.get(id = extra_info['dsl_id'] )
+    dsl = AssessmentKitDsl.objects.get(id=extra_info['dsl_id'])
     dsl.assessment_kit = assessment_kit
     dsl.save()
     return assessment_kit
+
 
 @transaction.atomic
 def __import_questionnaires(questionnaire_models, assessment_kit):
@@ -100,6 +108,7 @@ def __import_questionnaires(questionnaire_models, assessment_kit):
         questionnaire.assessment_kit = assessment_kit
         questionnaire.save()
 
+
 @transaction.atomic
 def __import_subjects(subject_models, assessment_kit):
     for model in subject_models:
@@ -111,12 +120,13 @@ def __import_subjects(subject_models, assessment_kit):
         subject.assessment_kit = assessment_kit
         questionnaire_codes = model['questionnaireCodes']
         subject.save()
-        if questionnaire_codes !=None:
+        if questionnaire_codes != None:
             for questionnaire_code in questionnaire_codes:
-                questionnaire = Questionnaire.objects.filter(code = questionnaire_code).first()
+                questionnaire = Questionnaire.objects.filter(code=questionnaire_code).first()
                 subject.questionnaires.add(questionnaire)
                 subject.save()
-        
+
+
 @transaction.atomic
 def __import_attributes(attributeModels):
     for model in attributeModels:
@@ -126,8 +136,9 @@ def __import_attributes(attributeModels):
         attribute.description = model['description']
         attribute.index = model['index']
         attribute.weight = model['weight']
-        attribute.assessment_subject = AssessmentSubject.objects.filter(code = model['subjectCode']).first()
+        attribute.assessment_subject = AssessmentSubject.objects.filter(code=model['subjectCode']).first()
         attribute.save()
+
 
 @transaction.atomic
 def __import_questions(questionModels, assessment_kit):
@@ -135,11 +146,12 @@ def __import_questions(questionModels, assessment_kit):
         question = Question()
         question.title = model['title']
         question.index = model['index']
+        question.description = model['description']
         if "mayNotBeApplicable" in model:
             question.may_not_be_applicable = model['mayNotBeApplicable']
         question.questionnaire = Questionnaire.objects.filter(code=model['questionnaireCode']).first()
         question.save()
-        
+
         for answer_model in model['answers']:
             answer = AnswerTemplate()
             answer.caption = answer_model['caption']
@@ -151,15 +163,15 @@ def __import_questions(questionModels, assessment_kit):
 
         for impact_model in model['questionImpacts']:
             impact = QuestionImpact()
-            impact.maturity_level = MaturityLevel.objects.get(assessment_kit = assessment_kit, title = impact_model['level']['title'])
-            impact.quality_attribute = QualityAttribute.objects.filter(code = impact_model['attributeCode']).first()
+            impact.maturity_level = MaturityLevel.objects.get(assessment_kit=assessment_kit,
+                                                              title=impact_model['level']['title'])
+            impact.quality_attribute = QualityAttribute.objects.filter(code=impact_model['attributeCode']).first()
             impact.question = question
             impact.weight = impact_model['weight']
             impact.save()
             subject = impact.quality_attribute.assessment_subject
             questionnaire = impact.question.questionnaire
             subject.questionnaires.add(questionnaire)
-            
 
             option_values_map = impact_model['optionValues']
             for option_number, option_value in option_values_map.items():
@@ -171,20 +183,19 @@ def __import_questions(questionModels, assessment_kit):
                         option_value_model.value = option_value
                 option_value_model.save()
                 impact.option_values.add(option_value_model)
-            
+
             impact.save()
 
-        
         question.save()
-            
+
 
 def get_dsl_file(assessment_kit):
-    try : 
+    try:
         result = {}
         dsl_file_path = assessment_kit.dsl.dsl_file.path
-        result["filename"] =  "assessmentkit.zip"
+        result["filename"] = "assessmentkit.zip"
         if os.path.isfile(dsl_file_path):
-            result["file"] = open(dsl_file_path,'rb')
+            result["file"] = open(dsl_file_path, 'rb')
             return ActionResult(success=True, data=result)
         return ActionResult(success=False, message='No such file exists in storage')
     except AssessmentKitDsl.DoesNotExist:
