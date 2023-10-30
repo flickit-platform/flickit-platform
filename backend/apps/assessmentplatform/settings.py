fc133f95a726eb3f39adf3fa2211e0a5104d0268
Khorsandi <khorsandi.mahnaz@gmail.com>
2023-10-30 18:32:25 +0330

    Release 1.3.2

--- backend/apps/assessmentplatform/settings.py
+++ backend/apps/assessmentplatform/settings.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "1.4.0-SNAPSHOT"
+__version__ = "1.3.2"
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
