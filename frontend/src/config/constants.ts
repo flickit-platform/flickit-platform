360617d32e2c766cab36ebbeb2db8aef885cbfba
amirrezaflh1402 <amirrezaflh1402@gmail.com>
2023-12-10 19:18:24 +0330

    OTAT-831

--- frontend/src/config/constants.ts
+++ frontend/src/config/constants.ts
@@ -1,6 +1,20 @@
 export const BASE_URL =
   process.env.NODE_ENV === "development"
-    ? import.meta.env.VITE_LOCAL_BASE_URL || "https://migration.flickit.org"
+    ? import.meta.env.VITE_LOCAL_BASE_URL || "https://test.flickit.org"
     : process.env.BASE_URL;
 
 export const APP_LABEL = "Flickit";
+
+export const KEYCLOACK_URL =
+  process.env.NODE_ENV === "development"
+    ? import.meta.env.VITE_LOCAL_BASE_URL || "https://test.flickit.org"
+    : process.env.KEYCLOACK_URL;
+export const KEYCLOACK_REALM =
+  process.env.NODE_ENV === "development"
+    ? "flickit"
+    : process.env.KEYCLOACK_URL;
+
+export const KEYCLOACK_CLIENT_ID =
+  process.env.NODE_ENV === "development"
+    ? "flickit-frontend"
+    : process.env.KEYCLOACK_CLIENT_ID;
