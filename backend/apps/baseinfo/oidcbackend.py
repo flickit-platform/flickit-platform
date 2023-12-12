dbfd4499034c7bf4d31ce1f1a2c2f56fc4364abc
alialibalaei <ali.alibalaii778@gmail.com>
2023-12-12 12:32:39 +0330

    sync Django user ID with keycloak when creating user.

--- backend/apps/baseinfo/oidcbackend.py
+++ backend/apps/baseinfo/oidcbackend.py
@@ -6,11 +6,12 @@ class MyOIDCAB(OIDCAuthenticationBackend):
 
     @transaction.atomic()
     def create_user(self, claims):
+        user_id = claims.get("sub")
         email = claims.get("email")
         first_name = claims.get('given_name', '')
         last_name = claims.get('family_name', '')
         display_name = first_name + ' ' + last_name
-        user = self.UserModel.objects.create_user(email, None, display_name)
+        user = self.UserModel.objects.create_user(email, None, display_name, id=user_id)
         spaceservices.create_default_space(user)
         return user
 
