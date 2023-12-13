4d7d14e55b0388574ce83a1088fc65ad03e01e79
Khorsandi <khorsandi.mahnaz@gmail.com>
2023-12-13 13:37:05 +0330

    Change version to 1.7.0-SNAPSHOT

--- backend/apps/assessmentplatform/settings.py
+++ backend/apps/assessmentplatform/settings.py
@@ -2,7 +2,7 @@ import os
 from datetime import timedelta
 from django.utils.translation import gettext_lazy as _
 
-__version__ = "1.6.1"
+__version__ = "1.7.0-SNAPSHOT"
 
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
