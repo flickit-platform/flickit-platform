73a798613771d880d78a93d7f77780eaae32b888
alialibalaei <ali.alibalaii778@gmail.com>
2024-01-20 13:57:11 +0330

    assessment kit creation service.

--- backend/apps/baseinfo/views/importassessmentkitviews.py
+++ backend/apps/baseinfo/views/importassessmentkitviews.py
@@ -15,7 +15,7 @@ from rest_framework.permissions import IsAuthenticated
 
 from assessmentplatform.settings import BASE_DIR, DSL_PARSER_URL_SERVICE
 
-from baseinfo.services import importassessmentkitservice, assessmentkitservice, dsl_services
+from baseinfo.services import importassessmentkitservice, assessmentkitservice, dsl_services, assessment_kit
 from baseinfo.serializers.assessmentkitserializers import ImportAssessmentKitSerializer, DslSerializer
 from baseinfo.permissions import IsMemberExpertGroup, IsOwnerExpertGroup
 
@@ -83,4 +83,13 @@ class ImportDslFileView(APIView):
         serializer.is_valid(raise_exception=True)
         result = dsl_services.upload_dsl_assessment(data=serializer.validated_data,
                                                     request=request)
+        return Response(data=result["body"], status=result["status_code"])
+
+
+class CreateAssessmentKitByDsl(APIView):
+    permission_classes = [IsAuthenticated]
+
+    def post(self, request):
+        result = assessment_kit.create_assessment_by_dsl(data=request.data,
+                                                         request=request)
         return Response(data=result["body"], status=result["status_code"])
