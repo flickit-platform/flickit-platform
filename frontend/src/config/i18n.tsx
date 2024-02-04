a2d1bc7d86033e76379e0d193b1679bfbf3d8407
amirrezaflh1402 <amirrezaflh1402@gmail.com>
2024-02-04 08:53:23 +0330

    OTAT-807

--- frontend/src/config/i18n.tsx
+++ frontend/src/config/i18n.tsx
@@ -1,21 +1,21 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import en from "./translations/en.json";
-import fr from "./translations/fr.json";
+import fa from "./translations/fa.json";
 
 const resources = {
   en: {
     translation: en,
   },
-  fr: {
-    translation: fr,
+  fa: {
+    translation: fa,
   },
 };
 
 i18n.use(initReactI18next).init({
   resources,
-  lng: "en",
-  fallbackLng: "en",
+  lng: "fa",
+  fallbackLng: "fa",
 
   interpolation: {
     escapeValue: false,
