89ec464b2d41d7cab756bc1645eda4c0a0efdfae
alialibalaei <ali.alibalaii778@gmail.com>
2024-02-05 09:12:58 +0330

    Create expert group creation service.

--- backend/apps/baseinfo/services/expert_group_services.py
+++ backend/apps/baseinfo/services/expert_group_services.py
@@ -2,10 +2,28 @@ import requests
 from assessmentplatform.settings import ASSESSMENT_URL
 
 
-def create_expert_group(request):
+def get_expert_group_list(request):
     response = requests.get(
-        ASSESSMENT_URL + f'assessment-core/api/expert-groups',
+        ASSESSMENT_URL + 'assessment-core/api/expert-groups',
         json=request.data,
         params=request.query_params,
         headers={'Authorization': request.headers['Authorization']})
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def create_expert_group(request):
+    if "picture" in request.data:
+        file = request.data['picture']
+        data = request.data
+        data.pop('picture')
+        response = requests.post(
+            ASSESSMENT_URL + 'assessment-core/api/expert-groups',
+            data=data,
+            files={'picture': file},
+            headers={'Authorization': request.headers['Authorization']})
+    else:
+        response = requests.post(
+            ASSESSMENT_URL + 'assessment-core/api/expert-groups',
+            data=request.data,
+            headers={'Authorization': request.headers['Authorization']})
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
