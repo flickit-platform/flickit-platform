89879726a0b13fc2e52e813f815bbc2fc1b74850
alialibalaei <ali.alibalaii778@gmail.com>
2024-02-06 12:18:14 +0330

    bug-fix change  request method to post.

--- backend/apps/assessment/services/advice_services.py
+++ backend/apps/assessment/services/advice_services.py
@@ -6,8 +6,8 @@ from assessmentplatform.settings import ASSESSMENT_URL
 
 def get_advice(request, assessment_id):
     result = dict()
-    response = requests.get(ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/advice',
-                            json=request.data, headers={'Authorization': request.headers['Authorization']})
+    response = requests.post(ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/advice',
+                             json=request.data, headers={'Authorization': request.headers['Authorization']})
     result["Success"] = False
     result["body"] = response.json()
     result["status_code"] = response.status_code
