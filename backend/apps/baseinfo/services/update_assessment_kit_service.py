ed1c3166abf9702e0c5585560b0ac4adc0f3aee9
alialibalaei <ali.alibalaii778@gmail.com>
2024-02-12 16:46:44 +0330

    bug-fix add header_authorization in update assessment kit update.

--- backend/apps/baseinfo/services/update_assessment_kit_service.py
+++ backend/apps/baseinfo/services/update_assessment_kit_service.py
@@ -16,17 +16,18 @@ def dsl_parser_upload_dsl(dsl_contents):
     return result
 
 
-def assessment_core_dsl_update(body, assessment_kit_id):
+def assessment_core_dsl_update(body, assessment_kit_id, header_authorization):
     data = {"dslContent": json.dumps(body)}
-    response = requests.put(ASSESSMENT_URL + f'assessment-core/api/assessment-kits/{assessment_kit_id}/update-by-dsl', json=data)
+    response = requests.put(ASSESSMENT_URL + f'assessment-core/api/assessment-kits/{assessment_kit_id}/update-by-dsl',
+                            json=data, headers={'Authorization': header_authorization})
     if response.status_code == status.HTTP_200_OK:
         return {"Success": True, "body": {"message": "The assessment kit updated successfully. "}, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
 
 
-def dsl_parser_update(assessment_kit_id, dsl_id):
+def dsl_parser_update(assessment_kit_id, dsl_id, header_authorization):
     dsl_contents = importassessmentkitservice.extract_dsl_contents(dsl_id)
     result = dsl_parser_upload_dsl(dsl_contents)
     if result["status_code"] == status.HTTP_200_OK:
-        result = assessment_core_dsl_update(result["body"], assessment_kit_id)
+        result = assessment_core_dsl_update(result["body"], assessment_kit_id, header_authorization)
     return result
