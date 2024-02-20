9e09033b976c3c342c2e4b203bcf4e28627fbba2
alialibalaei <ali.alibalaii778@gmail.com>
2024-02-20 14:40:41 +0330

    change request update_kit service.

--- backend/apps/baseinfo/services/update_assessment_kit_service.py
+++ backend/apps/baseinfo/services/update_assessment_kit_service.py
@@ -16,18 +16,10 @@ def dsl_parser_upload_dsl(dsl_contents):
     return result
 
 
-def assessment_core_dsl_update(body, assessment_kit_id, header_authorization):
-    data = {"dslContent": json.dumps(body)}
+def assessment_core_dsl_update(assessment_kit_id, dsl_id, header_authorization):
     response = requests.put(ASSESSMENT_URL + f'assessment-core/api/assessment-kits/{assessment_kit_id}/update-by-dsl',
-                            json=data, headers={'Authorization': header_authorization})
+                            json={"kitDslId": dsl_id}, headers={'Authorization': header_authorization})
     if response.status_code == status.HTTP_200_OK:
-        return {"Success": True, "body": {"message": "The assessment kit updated successfully. "}, "status_code": response.status_code}
+        return {"Success": True, "body": {"message": "The assessment kit updated successfully. "},
+                "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
-
-
-def dsl_parser_update(assessment_kit_id, dsl_id, header_authorization):
-    dsl_contents = importassessmentkitservice.extract_dsl_contents(dsl_id)
-    result = dsl_parser_upload_dsl(dsl_contents)
-    if result["status_code"] == status.HTTP_200_OK:
-        result = assessment_core_dsl_update(result["body"], assessment_kit_id, header_authorization)
-    return result
