28867f5fffe33a10c14f74d3c0503926c268ce74
alialibalaei <ali.alibalaii778@gmail.com>
2024-04-30 15:16:13 +0330

    adding check invitations to space first login.

--- backend/apps/baseinfo/oidcbackend.py
+++ backend/apps/baseinfo/oidcbackend.py
@@ -1,6 +1,8 @@
 from mozilla_django_oidc.auth import OIDCAuthenticationBackend
 from django.db import transaction
 
+from account.services.space_invite_services import check_invitations_spaces
+
 
 class MyOIDCAB(OIDCAuthenticationBackend):
 
@@ -12,4 +14,5 @@ class MyOIDCAB(OIDCAuthenticationBackend):
         last_name = claims.get('family_name', '')
         display_name = first_name + ' ' + last_name
         user = self.UserModel.objects.create_user(email, None, display_name, id=user_id)
+        check_invitations_spaces(user_id=user.id)
         return user
