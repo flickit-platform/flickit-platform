fdf80dc07c2c8d52aad3413147baafe797974dbd
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-01 11:47:04 +0330

    create space members list service.

--- backend/apps/account/views/space_views.py
+++ backend/apps/account/views/space_views.py
@@ -5,8 +5,12 @@ from rest_framework.response import Response
 from account.services import space_services
 
 
-class AddMemberInSpaceApi(APIView):
+class MembersSpaceApi(APIView):
     permission_classes = [IsAuthenticated]
+
+    def get(self, request, space_id):
+        result = space_services.get_list_members_in_space(request, space_id)
+        return Response(data=result["body"], status=result["status_code"])
 
     def post(self, request, space_id):
         result = space_services.add_member_in_space(request, space_id)
