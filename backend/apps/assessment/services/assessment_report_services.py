c37f99106d048753198c5703f604d3e2ec83700f
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-08 15:22:38 +0330

    create assessment subject report service version 2.

--- backend/apps/assessment/services/assessment_report_services.py
+++ backend/apps/assessment/services/assessment_report_services.py
@@ -4,8 +4,13 @@ from assessmentplatform.settings import ASSESSMENT_URL
 
 
 def get_assessment_report(request, assessment_id):
-    result = dict()
     response = requests.get(ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/report',
                             headers={'Authorization': request.headers['Authorization']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
 
+
+def get_assessment_subject_report(request, assessment_id, subject_id):
+    response = requests.get(ASSESSMENT_URL +
+                            f'assessment-core/api/assessments/{assessment_id}/report/subjects/{subject_id}',
+                            headers={'Authorization': request.headers['Authorization']})
+    return {"Success": True, "body": response.json(), "status_code": response.status_code}
