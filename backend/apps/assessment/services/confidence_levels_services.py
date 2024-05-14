c6eda202d5885e5f3a6bbab3f555752de1f21b08
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-14 11:32:02 +0330

    add authorization header in confidence levels services.

--- backend/apps/assessment/services/confidence_levels_services.py
+++ backend/apps/assessment/services/confidence_levels_services.py
@@ -3,13 +3,15 @@ import requests
 from assessmentplatform.settings import DSL_PARSER_URL_SERVICE, ASSESSMENT_URL
 
 
-def get_confidence_levels_in_assessment_core():
+def get_confidence_levels_in_assessment_core(request):
     response = requests.get(ASSESSMENT_URL + 'assessment-core/api/confidence-levels',
-                            )
+                            headers={'Authorization': request.headers['Authorization']})
+
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
 
 
-def get_confidence_levels_calculate_in_assessment_core(assessment_id):
+def get_confidence_levels_calculate_in_assessment_core(request, assessment_id):
     response = requests.post(ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/calculate-confidence',
-                            )
+                             headers={'Authorization': request.headers['Authorization']})
+
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
