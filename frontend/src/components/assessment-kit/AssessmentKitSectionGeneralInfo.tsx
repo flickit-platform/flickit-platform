50a5631bf39fbcb5b681d8eba6408aa852fcc222
mostafa.esmaeili <e.mostafa021@gmail.com>
2024-05-14 15:03:02 +0330

    OTAT-1192:

--- frontend/src/components/assessment-kit/AssessmentKitSectionGeneralInfo.tsx
+++ frontend/src/components/assessment-kit/AssessmentKitSectionGeneralInfo.tsx
@@ -495,7 +495,7 @@ const OnHoverInput = (props: any) => {
       config
     ) => service.updateAssessmentKitStats(args, config),
     runOnMount: false,
-    toastError: true,
+    // toastError: true,
   });
   const updateAssessmentKit = async () => {
     try {
@@ -504,6 +504,13 @@ const OnHoverInput = (props: any) => {
       await infoQuery();
     } catch (e) {
       const err = e as ICustomError;
+        if ("message" in err.response.data || {}) {
+            if (Array.isArray(err.response.data.message)) {
+                toastError(err.response.data.message[0]);
+            } else if (err.response.data.message) {
+                toastError(err.response.data.message);
+            }
+        }
       setError(err);
       setHasError(true);
     }
