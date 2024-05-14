15128f93a7921f20378403ba02e903e01351ca56
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-14 17:33:57 +0330

    remove the old maturity level calculate service.

--- backend/apps/assessment/url/url_assessment_v1.py
+++ backend/apps/assessment/url/url_assessment_v1.py
@@ -1,12 +1,12 @@
 from django.urls import path
 
 from assessment.views import (projectviews, questionvalueviews, questionaryviews, reportviews, confidence_levels_views,
-                              advice_views, attributes_views)
+                              advice_views, attributes_views, maturity_level_views)
 
 urlpatterns = [
     path("", projectviews.AssessmentProjectApi.as_view()),
     path("<uuid:assessment_id>/answer-question/", questionvalueviews.AnswerQuestionApi.as_view()),
-    path("<uuid:assessment_id>/calculate/", questionvalueviews.MaturityLevelCalculateApi.as_view()),
+    path("<uuid:assessment_id>/calculate/", maturity_level_views.MaturityLevelCalculateApi.as_view()),
     path("<uuid:assessment_id>/<int:questionnaire_id>/", questionvalueviews.LoadQuestionnaireAnswerApi.as_view()),
     path("<uuid:assessment_id>/questionnaires/", questionaryviews.LoadQuestionnairesWithAssessmentApi.as_view()),
     path("<uuid:assessment_id>/progress/", reportviews.AssessmentProgressApi.as_view()),
