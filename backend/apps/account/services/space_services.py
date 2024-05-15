dcfb5a943562b84449cde4ac1409ac4d8ee1324f
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-15 14:05:00 +0330

    create space list service.

--- backend/apps/account/services/space_services.py
+++ backend/apps/account/services/space_services.py
@@ -53,3 +53,11 @@ def get_space(request, space_id):
         json=request.data,
         headers={'Authorization': request.headers['Authorization']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
+
+
+def get_spaces_list(request):
+    response = requests.get(
+        ASSESSMENT_URL + 'assessment-core/api/spaces',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    return {"Success": True, "body": response.json(), "status_code": response.status_code}
