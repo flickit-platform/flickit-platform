57c40903d02ba0080d63ba6a7fe0552b75c3ef94
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-15 12:38:21 +0330

    create space member seen service.

--- backend/apps/account/url/url_space_v1.py
+++ backend/apps/account/url/url_space_v1.py
@@ -5,5 +5,6 @@ urlpatterns = [
     path("", space_views.SpaceApi.as_view()),
     path("<str:space_id>/members/", space_views.MembersSpaceApi.as_view()),
     path("<str:space_id>/invite/", space_views.InviteMemberInSpaceApi.as_view()),
+    path("<str:space_id>/seen/", space_views.SpaceSeenApi.as_view()),
 
 ]
