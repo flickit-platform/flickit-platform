e34b6dbeb7707d9f672234c4042ea6fd67ec75a3
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-25 15:21:16 +0330

    create questions list with questionnaire id service.

--- backend/apps/assessment/views/questionnaire_views.py
+++ backend/apps/assessment/views/questionnaire_views.py
@@ -1,8 +1,10 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from assessment.services import assessment_services
+from assessment.services import question_services, assessment_services
 
 
 class LoadQuestionnairesWithAssessmentApi(APIView):
@@ -12,3 +14,15 @@ class LoadQuestionnairesWithAssessmentApi(APIView):
         result = assessment_services.get_questionnaires_with_assessment_kit(request, assessment_id)
         return Response(data=result["body"], status=result["status_code"])
 
+
+class LoadQuestionsWithQuestionnairesApi(APIView):
+    permission_classes = [IsAuthenticated]
+    size_param = openapi.Parameter('size', openapi.IN_QUERY, description="size param",
+                                   type=openapi.TYPE_INTEGER)
+    page_param = openapi.Parameter('page', openapi.IN_QUERY, description="page param",
+                                   type=openapi.TYPE_INTEGER)
+
+    @swagger_auto_schema(manual_parameters=[size_param, page_param])
+    def get(self, request, assessment_id, questionnaire_id):
+        result = question_services.question_answering_list(request, assessment_id, questionnaire_id)
+        return Response(data=result["body"], status=result["status_code"])
