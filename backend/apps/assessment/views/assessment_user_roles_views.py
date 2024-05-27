f743eb53ca5933c78545022b3b48d31607ef8050
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-27 17:09:05 +0330

    create asessment user roles update service.

--- backend/apps/assessment/views/assessment_user_roles_views.py
+++ backend/apps/assessment/views/assessment_user_roles_views.py
@@ -1,3 +1,5 @@
+from drf_yasg import openapi
+from drf_yasg.utils import swagger_auto_schema
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
@@ -10,4 +12,16 @@ class AssessmentUserRolesApi(APIView):
 
     def get(self, request):
         result = assessment_user_roles_services.assessment_user_roles_list(request)
+        return Response(data=result["body"], status=result["status_code"])
+
+
+class UsersRolesInAssessmentApi(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT), responses={200: ""})
+    def put(self, request, assessment_id, user_id):
+        result = assessment_user_roles_services.add_user_role_in_assessment(request, assessment_id, user_id)
+        if result["Success"]:
+            return Response(status=result["status_code"])
         return Response(data=result["body"], status=result["status_code"])
