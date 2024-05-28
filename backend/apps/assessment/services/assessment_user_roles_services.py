50957e649ccfb9618aae165e18c1799e591959fe
alialibalaei <ali.alibalaii778@gmail.com>
2024-05-28 14:43:22 +0330

    create asessment user roles creation service.

--- backend/apps/assessment/services/assessment_user_roles_services.py
+++ backend/apps/assessment/services/assessment_user_roles_services.py
@@ -10,12 +10,23 @@ def assessment_user_roles_list(request):
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
 
 
-def add_user_role_in_assessment(request, assessment_id, user_id):
+def update_user_role_in_assessment(request, assessment_id, user_id):
     response = requests.put(
         ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/assessment-user-roles/{user_id}',
         json=request.data,
         headers={'Authorization': request.headers['Authorization']})
 
     if response.status_code == 200:
+        return {"Success": True, "body": "", "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def add_user_role_in_assessment(request, assessment_id):
+    response = requests.post(
+        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/assessment-user-roles',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+
+    if response.status_code == 201:
         return {"Success": True, "body": "", "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
