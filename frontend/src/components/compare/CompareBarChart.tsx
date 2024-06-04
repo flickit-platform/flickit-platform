0536908a508cb9b4bfc3aa3887d9afc70981bdd6
mahdasjs <mahdasjs@gmail.com>
2024-06-04 21:02:23 +0330

    OTAT-1277: change bar color

--- frontend/src/components/compare/CompareBarChart.tsx
+++ frontend/src/components/compare/CompareBarChart.tsx
@@ -17,7 +17,7 @@ interface CompareBarProps {
   assessmentCount: number;
 }
 
-const barColors = ["#B4D4FF", "#98ABEE", "#0E46A3", "#1E0342"];
+const barColors = ["#82A6CB", "#98ABEE", "#0E46A3", "#1E0342"];
 
 const CustomTooltip: React.FC<TooltipProps<number, string>> = ({
   active,
