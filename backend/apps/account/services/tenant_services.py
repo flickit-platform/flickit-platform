fdf3d7e4f6f5fcd717f6a8cbf1701606890473d2
alialibalaei <ali.alibalaii778@gmail.com>
2024-06-23 13:35:11 +0330

    create tenant logo service.

--- backend/apps/account/services/tenant_services.py
+++ backend/apps/account/services/tenant_services.py
@@ -6,3 +6,9 @@ def tenant_info():
     response = requests.get(
         ASSESSMENT_URL + 'assessment-core/api/tenant/info')
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
+
+
+def tenant_logo():
+    response = requests.get(
+        ASSESSMENT_URL + 'assessment-core/api/tenant/logo')
+    return {"Success": True, "body": response.json(), "status_code": response.status_code}
