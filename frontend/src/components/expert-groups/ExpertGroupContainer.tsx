09e4b86c7060115d5709c65a4a1628a43b01487c
mahdasjs@gmail.com <mahdasjs@gmail.com>
2024-06-27 14:01:32 +0330

    OTAT-1344: display assessment kit details instead of market place

--- frontend/src/components/expert-groups/ExpertGroupContainer.tsx
+++ frontend/src/components/expert-groups/ExpertGroupContainer.tsx
@@ -96,7 +96,12 @@ const ExpertGroupContainer = () => {
           editable,
           assessment_kits = [],
         } = data || {};
-        // const is_owner = data?.owner?.id === userInfo.id;
+        const is_member = expertGroupMembersQueryData.data?.items?.some(
+          (res: any) => {
+            return res.id === userInfo.id;
+          }
+        );
+        console.log(is_member)
         const hasAccess = editable;
         setDocTitle(`${t("expertGroup")}: ${title || ""}`);
         return (
@@ -146,7 +151,7 @@ const ExpertGroupContainer = () => {
                     queryData={queryData}
                     hasAccess={editable}
                     dialogProps={createAssessmentKitDialogProps}
-                    is_member={editable}
+                    is_member={is_member}
                     is_expert={editable}
                     setAssessmentKitsCounts={setAssessmentKitsCounts}
                   />
@@ -774,7 +779,7 @@ const AssessmentKitsList = (props: any) => {
                         link={
                           is_member
                             ? `assessment-kits/${assessment_kit?.id}`
-                            : `/assessment-kits`
+                            : `/assessment-kits/${assessment_kit?.id}`
                         }
                         key={assessment_kit?.id}
                         data={assessment_kit}
