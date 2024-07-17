43435fadb93086ad73ec4f06642fb7608f00ade7
mahdasjs <mahdasjs@gmail.com>
2024-07-17 16:49:38 +0330

    change sentry environment

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -4,6 +4,7 @@ interface ImportMetaEnv {
   VITE_SSO_REALM: string;
   VITE_SSO_CLIENT_ID: string;
   VITE_SENTRY_DSN: string;
+  VITE_SENTRY_ENVIRONMENT: string;
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv;
