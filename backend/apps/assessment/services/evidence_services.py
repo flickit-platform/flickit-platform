118e1a511448d843a8cfcf4724f3d6f60b109fcf
alialibalaei <ali.alibalaii778@gmail.com>
2024-07-21 11:58:56 +0330

    Create evidence attachment deletion service.

--- backend/apps/assessment/services/evidence_services.py
+++ backend/apps/assessment/services/evidence_services.py
@@ -53,3 +53,12 @@ def evidence_list_attachments(request, evidence_id):
         ASSESSMENT_URL + f'assessment-core/api/evidences/{evidence_id}/attachments',
         headers={'Authorization': request.headers['Authorization']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
+
+
+def evidence_delete_attachment(request, evidence_id, attachment_id):
+    response = requests.delete(
+        ASSESSMENT_URL + f'assessment-core/api/evidences/{evidence_id}/attachments/{attachment_id}',
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 204:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
--- backend/apps/assessment/url/url_evidences.py
+++ backend/apps/assessment/url/url_evidences.py
@@ -4,4 +4,5 @@ urlpatterns = [
     path("", evidenceviews.EvidencesApi.as_view()),
     path("<uuid:evidence_id>/", evidenceviews.EvidenceApi.as_view()),
     path("<uuid:evidence_id>/attachments/", evidenceviews.EvidenceAttachmentsApi.as_view()),
+    path("<uuid:evidence_id>/attachments/<uuid:attachment_id>/", evidenceviews.EvidenceAttachmentApi.as_view()),
 ]
