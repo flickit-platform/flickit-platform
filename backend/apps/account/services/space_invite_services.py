819943b2b7c6a08020b3a7cc2765089693eaf417
alialibalaei <ali.alibalaii778@gmail.com>
2024-07-21 19:34:49 +0330

    Add assessment invitation checking in user creation.

--- backend/apps/account/services/space_invite_services.py
+++
@@ -1,8 +0,0 @@
-import requests
-from assessmentplatform.settings import ASSESSMENT_URL
-
-
-def check_invitations_spaces(user_id):
-    response = requests.put(
-        ASSESSMENT_URL + 'assessment-core/api/spaces-accept-invitations',
-        json={"userId": user_id})
