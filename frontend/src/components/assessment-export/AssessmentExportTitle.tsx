6ee354d4af2fe0980aea809743d075f5df4c5a2a
mahdasjs <mahdasjs@gmail.com>
2024-07-23 18:59:28 +0330

    OTAT-1399: Scetch a sample exporting report

---
+++ frontend/src/components/assessment-export/AssessmentExportTitle.tsx
@@ -0,0 +1,51 @@
+import React, { useEffect } from "react";
+import Title from "@common/TitleComponent";
+import SupTitleBreadcrumb from "@/components/common/SupTitleBreadcrumb";
+import { useParams } from "react-router-dom";
+
+interface IAssessmentExportTitle {
+  pathInfo: {
+      space: {
+          id: number,
+          title: string
+      },
+      assessment: {
+          id: string,
+          title: string
+      }
+  };
+}
+
+const AssessmentExportTitle = (props: IAssessmentExportTitle) => {
+  const { pathInfo } = props;
+  const { spaceId, page } = useParams();
+  const { space, assessment } = pathInfo;
+
+  return (
+    <Title
+      backLink="/"
+      wrapperProps={{
+        sx: {
+          flexDirection: { xs: "column", md: "row" },
+          alignItems: { xs: "flex-start", md: "flex-end" },
+        },
+      }}
+      sup={
+        <SupTitleBreadcrumb
+          routes={[
+            {
+              title: space?.title,
+              to: `/${spaceId}/assessments/${page}`,
+            },
+            {
+              title: assessment?.title,
+            },
+          ]}
+          displayChip
+        />
+      }
+    ></Title>
+  );
+};
+
+export default AssessmentExportTitle;
