ab41afde078ffee2d0f8cca7fe84719463e99d21
alialibalaei <ali.alibalaii778@gmail.com>
2024-07-26 20:46:18 +0330

    remove old urls.

--- backend/apps/account/urls.py
+++ backend/apps/account/urls.py
@@ -3,9 +3,7 @@ from rest_framework_nested import routers
 from rest_framework_simplejwt import views
 from rest_framework_simplejwt.views import TokenObtainPairView
 
-from assessment.views import projectviews
 
-from account.views.spaceviews import ChangeCurrentSpaceViewSet
 from account.serializers.authserializers import MyTokenObtainPairSerializer
 
 router = routers.DefaultRouter()
@@ -18,5 +16,4 @@ urlpatterns += [
             name="jwt-create"),
     re_path(r"^jwt/refresh/?", views.TokenRefreshView.as_view(), name="jwt-refresh"),
     re_path(r"^jwt/verify/?", views.TokenVerifyView.as_view(), name="jwt-verify"),
-    path('changecurrentspace/<str:space_id>/', ChangeCurrentSpaceViewSet.as_view()),
 ]
