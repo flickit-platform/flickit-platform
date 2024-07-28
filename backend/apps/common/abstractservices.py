d2c7d26ff046a45c964f85d358167c752904be1c
alialibalaei <ali.alibalaii778@gmail.com>
2024-07-28 13:01:56 +0330

    remove common def.

--- backend/apps/common/abstractservices.py
+++
@@ -1,7 +0,0 @@
-from django.core.exceptions import ObjectDoesNotExist
-
-def load_model(model_class, pk):
-    try:
-        return model_class.objects.get(pk=pk)
-    except ObjectDoesNotExist:
-        raise model_class.DoesNotExist
--- backend/apps/common/validators.py
+++ backend/apps/common/validators.py
@@ -1,10 +1,12 @@
 from django.core.exceptions import ValidationError
 
+
 def validate_file_size(value):
     filesize = value.size
     if filesize > 5 * 1024 * 1024:
         raise ValidationError("File size must be no larger than 5 MB")
     return value
+
 
 def validate_dsl_extension(value):
     import os
