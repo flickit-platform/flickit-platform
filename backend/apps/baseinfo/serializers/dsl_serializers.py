654e5331540909c0191898d279b1d129a82642e7
alialibalaei <ali.alibalaii778@gmail.com>
2024-08-21 14:53:35 +0330

    OTAT-1542: Change ValidationError and add size validtion.

--- backend/apps/baseinfo/serializers/dsl_serializers.py
+++ backend/apps/baseinfo/serializers/dsl_serializers.py
@@ -5,7 +5,13 @@ class ExcelFileUploadSerializer(serializers.Serializer):
     file = serializers.FileField()
 
     def validate_file(self, value):
+        # Check that the file has a .xlsx extension
         if not value.name.endswith('.xlsx'):
-            raise serializers.ValidationError("فقط فایل‌های اکسل با فرمت .xlsx پشتیبانی می‌شوند.")
+            raise serializers.ValidationError("Only Excel files with .xlsx extension are supported.")
+
+        # Check that the file size does not exceed 5MB
+        max_size_mb = 5
+        if value.size > max_size_mb * 1024 * 1024:
+            raise serializers.ValidationError(f"File size should not exceed {max_size_mb} MB.")
 
         return value
