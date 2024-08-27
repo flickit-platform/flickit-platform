ae2dda2977ce83d2cce7329e0ebe376cbde8cad4
alialibalaei <ali.alibalaii778@gmail.com>
2024-08-28 00:24:46 +0330

    OTAT-1552:create assessment subject insights service.

--- backend/apps/assessment/services/assessment_insight_services.py
+++ backend/apps/assessment/services/assessment_insight_services.py
@@ -3,10 +3,20 @@ import requests
 from assessmentplatform.settings import ASSESSMENT_URL
 
 
-def update_assessment_insights(request, assessment_id):
+def create_assessment_insights(request, assessment_id):
     response = requests.post(ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/insights',
                              json=request.data,
                              headers={'Authorization': request.headers['Authorization']})
     if response.status_code == 201:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def create_assessment_subject_insights(request, assessment_id, subject_id):
+    response = requests.post(
+        ASSESSMENT_URL + f'assessment-core/api/assessments/{assessment_id}/insights/subjects/{subject_id}',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 200:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
--- backend/apps/assessment/views/assessment_insight_views.py
+++ backend/apps/assessment/views/assessment_insight_views.py
@@ -12,7 +12,19 @@ class AssessmentInsightApi(APIView):
     @swagger_auto_schema(request_body=openapi.Schema(
         type=openapi.TYPE_OBJECT), responses={201: ""})
     def post(self, request, assessment_id):
-        result = assessment_insight_services.update_assessment_insights(request, assessment_id)
+        result = assessment_insight_services.create_assessment_insights(request, assessment_id)
+        if result["Success"]:
+            return Response(status=result["status_code"])
+        return Response(data=result["body"], status=result["status_code"])
+
+
+class AssessmentSubjectInsightApi(APIView):
+    permission_classes = [IsAuthenticated]
+
+    @swagger_auto_schema(request_body=openapi.Schema(
+        type=openapi.TYPE_OBJECT), responses={200: ""})
+    def post(self, request, assessment_id, subject_id):
+        result = assessment_insight_services.create_assessment_subject_insights(request, assessment_id, subject_id)
         if result["Success"]:
             return Response(status=result["status_code"])
         return Response(data=result["body"], status=result["status_code"])
