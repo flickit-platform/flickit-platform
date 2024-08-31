f75641def9a87deb78a1b829f8dfea53f56d9c06
alialibalaei <ali.alibalaii778@gmail.com>
2024-08-31 14:23:21 +0330

    OTAT-1555: Add assessment permissions list in subject report service.

--- backend/apps/assessment/views/assessment_report_views.py
+++ backend/apps/assessment/views/assessment_report_views.py
@@ -2,7 +2,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.views import APIView
 from rest_framework.response import Response
 
-from assessment.services import assessment_report_services
+from assessment.services import assessment_report_services, assessment_permission_services
 
 
 class AssessmentReportApi(APIView):
@@ -17,7 +17,11 @@ class AssessmentSubjectReportApi(APIView):
     permission_classes = [IsAuthenticated]
 
     def get(self, request, assessment_id, subject_id):
+        permissions_result = assessment_permission_services.get_assessment_permissions_list(request, assessment_id)
         result = assessment_report_services.get_assessment_subject_report(request, assessment_id, subject_id)
+        if result["status_code"] == 200 and permissions_result["status_code"] == 200:
+            result["body"]["permissions"] = permissions_result["body"]["permissions"]
+            return Response(data=result["body"], status=result["status_code"])
         return Response(data=result["body"], status=result["status_code"])
 
 
