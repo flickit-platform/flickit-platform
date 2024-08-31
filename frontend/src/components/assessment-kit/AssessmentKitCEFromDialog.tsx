c914be21726a1545cd1d4b84cb494f35ce060e22
mostafa.esmaeili <e.mostafa021@gmail.com>
2024-08-31 16:34:08 +0330

    OTAT-1547: fix

--- frontend/src/components/assessment-kit/AssessmentKitCEFromDialog.tsx
+++ frontend/src/components/assessment-kit/AssessmentKitCEFromDialog.tsx
@@ -25,7 +25,7 @@ import convertToBytes from "@/utils/convertToBytes";
 
 interface IAssessmentKitCEFromDialogProps extends DialogProps {
   onClose: () => void;
-  onSubmitForm: () => void;
+  onSubmitForm?: () => void;
   openDialog?: any;
   context?: any;
 }
