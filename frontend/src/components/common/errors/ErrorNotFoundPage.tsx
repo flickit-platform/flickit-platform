ff926eb8b520f551b08382e95ddb3a5340ed4ddf
mostafa.esmaeili <e.mostafa021@gmail.com>
2024-09-30 09:00:53 +0330

    OTAT-1672: add navbar to 404

--- frontend/src/components/common/errors/ErrorNotFoundPage.tsx
+++ frontend/src/components/common/errors/ErrorNotFoundPage.tsx
@@ -1,18 +1,24 @@
 import Box from "@mui/material/Box";
 import PageNotFoundImage from "@assets/svg/404.svg";
 import { styles } from "@styles";
+import Navbar from "@common/Navbar";
+import React from "react";
 
 const ErrorNotFoundPage = () => {
   return (
-    <Box
-      sx={{ ...styles.centerCVH }}
-      textAlign="center"
-      height="calc(100vh - 264px)"
-    >
-      <Box sx={{ width: { xs: "100vw", sm: "80vw", md: "60vw", lg: "50vw" } }}>
-        <img src={PageNotFoundImage} alt={"page not found"} width="100%" />
-      </Box>
-    </Box>
+      <>
+        <Navbar />
+        <Box
+            sx={{ ...styles.centerCVH }}
+            textAlign="center"
+            height="calc(100vh - 264px)"
+        >
+          <Box sx={{ width: { xs: "100vw", sm: "80vw", md: "60vw", lg: "50vw" } }}>
+            <img src={PageNotFoundImage} alt={"page not found"} width="100%" />
+          </Box>
+        </Box>
+      </>
+
   );
 };
 
