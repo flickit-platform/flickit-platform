ba7e0dcf36a8ebd0d10444e187240c0c5aee1e8a
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-02 17:57:47 +0330

    OTAT-1665: Creating a service to create subject in kit.

---
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -0,0 +1,9 @@
+import requests
+from assessmentplatform.settings import ASSESSMENT_URL
+
+def create_subject_kit_version(request,kit_version_id):
+    response = requests.post(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/subjects',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    return {"Success": True, "body": response.json(), "status_code": response.status_code}
