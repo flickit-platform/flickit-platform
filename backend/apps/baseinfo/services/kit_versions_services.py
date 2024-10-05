d4d5030906a67797c422ea1a7697a5365d86b678
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-05 10:31:43 +0330

    OTAT-1686: Creating a service to deletion maturity levels in kit.

--- backend/apps/baseinfo/services/kit_versions_services.py
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -26,3 +26,12 @@ def update_maturity_level_with_kit_version(request, kit_version_id, maturity_lev
     if response.status_code == 200:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def delete_maturity_level_with_kit_version(request, kit_version_id, maturity_level_id):
+    response = requests.delete(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/maturity-levels/{maturity_level_id}',
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 204:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
