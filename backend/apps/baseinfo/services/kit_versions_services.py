4d4315008313349b91a0ab0e4c8ef58ca0f0ff57
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-05 09:25:51 +0330

    OTAT-1680: Creating a service to update maturity levels in kit.

--- backend/apps/baseinfo/services/kit_versions_services.py
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -1,7 +1,8 @@
 import requests
 from assessmentplatform.settings import ASSESSMENT_URL
 
-def create_subject_kit_version(request,kit_version_id):
+
+def create_subject_kit_version(request, kit_version_id):
     response = requests.post(
         ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/subjects',
         json=request.data,
@@ -15,3 +16,13 @@ def create_maturity_levels_with_kit_version(request, kit_version_id):
         json=request.data,
         headers={'Authorization': request.headers['Authorization']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
+
+
+def update_maturity_level_with_kit_version(request, kit_version_id, maturity_leve_id):
+    response = requests.put(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/maturity-levels/{maturity_leve_id}',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 200:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
