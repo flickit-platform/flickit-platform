3d072a1a136b55144fe2ce4baa06a422de0eccc6
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-09 17:07:55 +0330

    OTAT-1690: Creating a service to chagne maturity level order.

--- backend/apps/baseinfo/url/url_kit_versions_v1.py
+++ backend/apps/baseinfo/url/url_kit_versions_v1.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path("<str:kit_version_id>/maturity-levels/", kit_versions_views.KitVersionMaturityLevelsApi.as_view()),
     path("<str:kit_version_id>/maturity-levels/<str:maturity_level_id>",
          kit_versions_views.KitVersionMaturityLevelApi.as_view()),
+    path("<str:kit_version_id>/maturity-levels-change-order/",
+         kit_versions_views.MaturityLevelsChangeOrderApi.as_view()),
 ]
