bac96944899a851c67639a680f68d51de64f9b74
mahdasjs@gmail.com <mahdasjs@gmail.com>
2024-10-09 10:57:38 +0330

    OTAT-1671: fix packaging in setting page

--- frontend/src/components/common/settingBox.tsx
+++ frontend/src/components/common/settingBox.tsx
@@ -1,6 +1,13 @@
 import React, {useRef, useState} from 'react';
 import Box from "@mui/material/Box";
-import {Chip, CircularProgress, Divider, FormControl, IconButton, TextField, Tooltip, Typography} from "@mui/material";
+import Chip from "@mui/material/Chip";
+import CircularProgress from "@mui/material/CircularProgress";
+import Divider from "@mui/material/Divider";
+import FormControl from "@mui/material/FormControl";
+import IconButton from "@mui/material/IconButton";
+import TextField from "@mui/material/TextField";
+import Tooltip from "@mui/material/Tooltip";
+import Typography from "@mui/material/Typography";
 import {Trans} from "react-i18next";
 import Button from "@mui/material/Button";
 import AddIcon from "@mui/icons-material/Add";
