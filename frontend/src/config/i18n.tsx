d0a2c69ae497d57a32b0722663f13e26fa834df1
mahdasjs@gmail.com <mahdasjs@gmail.com>
2024-10-12 10:36:39 +0330

    OTAT-1725: fix translation bug

--- frontend/src/config/i18n.tsx
+++ frontend/src/config/i18n.tsx
@@ -11,8 +11,8 @@ export const resources = {
     translation: fa,
   },
 };
-const storedLanguage = localStorage.getItem("lang") || "en";
-document.dir = storedLanguage === "fa" ? "rtl" : "ltr";
+const storedLanguage = "en";
+document.dir = "ltr";
 i18n.use(initReactI18next).init({
   resources,
   lng: storedLanguage,
