19bd43f36ecda5dc585ea0b3f820e40d2938c2e0
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-27 19:03:40 +0330

    OTAT-1761: Creating a service to delete questionnaire.

--- backend/apps/baseinfo/services/kit_versions_services.py
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -187,3 +187,12 @@ def update_questionnaire(request, kit_version_id, questionnaire_id):
     if response.status_code == 200:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def delete_questionnaire(request, kit_version_id, questionnaire_id):
+    response = requests.delete(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/questionnaires/{questionnaire_id}',
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 204:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
