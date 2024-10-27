f76ccfcffb502fa90cb0a47c616a45195ea48f7f
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-27 17:31:52 +0330

    OTAT-1678: Creating a service to create questionnaire

--- backend/apps/baseinfo/services/kit_versions_services.py
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -161,3 +161,11 @@ def get_attributes_list(request, kit_version_id):
         params=request.query_params,
         headers={'Authorization': request.headers['Authorization']})
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
+
+
+def create_questionnaire(request, kit_version_id):
+    response = requests.post(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/questionnaires',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    return {"Success": True, "body": response.json(), "status_code": response.status_code}
