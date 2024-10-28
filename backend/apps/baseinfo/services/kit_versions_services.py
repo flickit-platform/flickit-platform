23db1fd07c932c318fce0542fa3eeaa10479eac8
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-28 10:58:41 +0330

    OTAT-1748: Creating a service to chagne questionnaires order.

--- backend/apps/baseinfo/services/kit_versions_services.py
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -196,3 +196,13 @@ def delete_questionnaire(request, kit_version_id, questionnaire_id):
     if response.status_code == 204:
         return {"Success": True, "body": None, "status_code": response.status_code}
     return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
+def change_questionnaire_order(request, kit_version_id):
+    response = requests.put(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/questionnaires-change-order',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 200:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
