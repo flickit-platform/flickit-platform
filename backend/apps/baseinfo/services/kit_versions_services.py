b55409a405e20114f824df6caad5692ce4619efb
alialibalaei <ali.alibalaii778@gmail.com>
2024-10-28 11:53:10 +0330

    OTAT-1749: Creating a service to update attribute.

--- backend/apps/baseinfo/services/kit_versions_services.py
+++ backend/apps/baseinfo/services/kit_versions_services.py
@@ -163,6 +163,16 @@ def get_attributes_list(request, kit_version_id):
     return {"Success": True, "body": response.json(), "status_code": response.status_code}
 
 
+def update_questionnaire(request, kit_version_id, attribute_id):
+    response = requests.put(
+        ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/attributes/{attribute_id}',
+        json=request.data,
+        headers={'Authorization': request.headers['Authorization']})
+    if response.status_code == 200:
+        return {"Success": True, "body": None, "status_code": response.status_code}
+    return {"Success": False, "body": response.json(), "status_code": response.status_code}
+
+
 def create_questionnaire(request, kit_version_id):
     response = requests.post(
         ASSESSMENT_URL + f'assessment-core/api/kit-versions/{kit_version_id}/questionnaires',
