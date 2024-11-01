9b091d88e66205cf88be60fca398baef8a44b8c1
mahdasjs@gmail.com <mahdasjs@gmail.com>
2024-11-01 23:59:58 +0330

    OTAT-1727: download instead of coping when access denied

--- frontend/src/utils/handleCopy.tsx
+++ frontend/src/utils/handleCopy.tsx
@@ -1,27 +1,39 @@
 import html2canvas from "html2canvas";
+import { copyImageToClipboard } from "copy-image-clipboard";
 import { toast } from "react-toastify";
 
 export const handleCopyAsImage = async (
-    element: HTMLDivElement | null,
-    setLoading: (loading: boolean) => void,
-  ) => {
-    if (element) {
-      setLoading(true); 
-      try {
-        const canvas = await html2canvas(element);
-        canvas.toBlob(async (blob) => {
-          if (blob) {
-            const item = new ClipboardItem({ "image/png": blob });
-            await navigator.clipboard.write([item]);
-            toast.success("Chart content copied as an image!");
-          } else {
-            console.error("Failed to create blob from canvas.");
-          }
-        });
-      } catch (err) {
-        console.error("Failed to copy image to clipboard:", err);
-      } finally {
-        setLoading(false);
-      }
+  element: HTMLDivElement | null,
+  setLoading: (loading: boolean) => void,
+  title?: any,
+) => {
+  if (!element) return;
+
+  setLoading(true);
+  const canvas = await html2canvas(element, { useCORS: true });
+  const dataUrl: any = canvas.toDataURL("image/png");
+
+  try {
+    await copyImageToClipboard(dataUrl);
+    toast.success("Chart content copied as an image!");
+  } catch (error: any) {
+    if (error.name === "NotAllowedError") {
+      toast.error("Clipboard access denied. Image will be downloaded instead.");
+    } else {
+      console.error("Failed to copy image to clipboard:", error);
+      toast.error(
+        "Failed to copy image to clipboard. Image will be downloaded instead.",
+      );
     }
-  };
+
+    const blob = await (await fetch(dataUrl)).blob();
+    const url = URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = url;
+    link.download = `${title ? title : "chart"}.png`;
+    link.click();
+    URL.revokeObjectURL(url);
+  } finally {
+    setLoading(false);
+  }
+};
