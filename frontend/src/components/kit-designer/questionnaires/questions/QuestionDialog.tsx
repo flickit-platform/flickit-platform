7c525223a8f530eb92a14e9c6140517d693fd4dd
mahdasjs@gmail.com <mahdasjs@gmail.com>
2024-11-09 01:18:34 +0330

    OTAT-1815: Translate all parts

--- frontend/src/components/kit-designer/questionnaires/questions/QuestionDialog.tsx
+++ frontend/src/components/kit-designer/questionnaires/questions/QuestionDialog.tsx
@@ -1,15 +1,12 @@
 import React, { useState, useEffect } from "react";
 import {
-  Dialog,
-  DialogTitle,
-  DialogContent,
   Grid,
   TextField,
-  Button,
   IconButton,
   Typography,
   Switch,
   Box,
+  Button,
 } from "@mui/material";
 import { Trans } from "react-i18next";
 import { useForm, FormProvider } from "react-hook-form";
@@ -18,6 +15,14 @@ import CloseIcon from "@mui/icons-material/Close";
 import { useServiceContext } from "@/providers/ServiceProvider";
 import { IQuestionInfo } from "@/types";
 import { useQuery } from "@/utils/useQuery";
+import {
+  CEDialog,
+  CEDialogActions,
+} from "@/components/common/dialogs/CEDialog";
+import FormProviderWithForm from "@/components/common/FormProviderWithForm";
+import { t } from "i18next";
+import { styles } from "@styles";
+import EmptyState from "../../common/EmptyState";
 
 interface QuestionDialogProps {
   open: boolean;
@@ -40,23 +45,14 @@ const QuestionDialog: React.FC<QuestionDialogProps> = ({
     runOnMount: false,
   });
 
-  useEffect(() => {}, []);
   const [questionData, setQuestionData] = useState<any>(null);
-  const formMethods = useForm({
-    defaultValues: {
-      title: question?.title || "",
-      hint: question?.hint || "",
-      options: question?.options || [{ text: "" }], // Default to an array with one empty option if none exist
-      mayNotBeApplicable: question?.mayNotBeApplicable || false,
-      advisable: question?.advisable || false,
-    },
-  });
+  const formMethods = useForm({ shouldUnregister: true });
+
   const { service } = useServiceContext();
 
   useEffect(() => {
     if (open && question.id) {
       fetchAttributeKit.query();
-      console.log(fetchAttributeKit.data)
       formMethods.reset({
         title: question?.title || "",
         hint: question?.hint || "",
@@ -71,7 +67,7 @@ const QuestionDialog: React.FC<QuestionDialogProps> = ({
     try {
       const requestData = {
         ...data,
-        index: 1, // Ensure index is included in the request payload
+        index: 1,
       };
       await service.updateQuestionsKit({
         kitVersionId,
@@ -85,133 +81,137 @@ const QuestionDialog: React.FC<QuestionDialogProps> = ({
     }
   };
 
+  const handleAddNewRow = () => {};
+
   return (
-    <Dialog open={open} onClose={onClose} maxWidth="md" fullWidth>
-      <DialogTitle>
-        <Box display="flex" alignItems="center">
-          <Typography variant="h6">
-            <Trans i18nKey="addNewQuestion" />
+    <CEDialog
+      open={open}
+      onClose={onClose}
+      title={<Trans i18nKey="addNewQuestion" />}
+    >
+      <FormProviderWithForm
+        formMethods={formMethods}
+        style={{
+          paddingBlock: 20,
+          paddingInline: 40,
+        }}
+      >
+        <Box display="flex" flexDirection="column" gap={1}>
+          <Typography variant="semiBoldXLarge" gutterBottom>
+            <Trans i18nKey="questionAndOptions" />
+          </Typography>
+          <Typography variant="bodyMedium" color="textSecondary">
+            <Trans i18nKey="questionAndOptionsDescription" />
           </Typography>
         </Box>
-      </DialogTitle>
-      <DialogContent>
-        <FormProvider {...formMethods}>
-          <form onSubmit={formMethods.handleSubmit(onSubmit)}>
-            {/* Section 1: Question and Options */}
-            <Typography variant="subtitle1" gutterBottom>
-              <Trans i18nKey="questionAndOptions" />
+        <Grid container spacing={2} mt={2}>
+          <Grid item xs={12}>
+            <TextField
+              {...formMethods.register("title", { required: true })}
+              fullWidth
+              label="Question"
+              placeholder={t("questionPlaceholder").toString()}
+              required
+              sx={{
+                "& .MuiInputBase-root": {
+                  height: 46,
+                },
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <TextField
+              {...formMethods.register("hint")}
+              fullWidth
+              label="Hint"
+              placeholder={t("hintPlaceholder").toString()}
+              sx={{
+                "& .MuiInputBase-root": {
+                  height: 46,
+                },
+              }}
+            />
+          </Grid>
+          <Grid item xs={12}>
+            <Typography variant="body2">
+              <Trans i18nKey="answerOptions" />
             </Typography>
-            <Typography variant="body2" color="textSecondary">
-              In this section, enter the question text and its options. You can
-              also add a hint.
-            </Typography>
-            <Grid container spacing={2}>
-              <Grid item xs={12}>
+            {/* {formMethods.watch("options").map((option: any, index: number) => (
+              <Box display="flex" alignItems="center" mt={1} key={index}>
                 <TextField
-                  {...formMethods.register("title", { required: true })}
                   fullWidth
-                  label="Question"
-                  placeholder="Write the question here"
-                  required
+                  placeholder={`Option ${index + 1}`}
+                  defaultValue={option.text}
                 />
-              </Grid>
-              <Grid item xs={12}>
-                <TextField
-                  {...formMethods.register("hint")}
-                  fullWidth
-                  label="Hint"
-                  placeholder="Leave a hint for assessors"
-                />
-              </Grid>
-              <Grid item xs={12}>
-                <Typography variant="body2">
-                  <Trans i18nKey="answerOptions" />
-                </Typography>
-                {formMethods
-                  .watch("options")
-                  .map((option: any, index: number) => (
-                    <Box display="flex" alignItems="center" mt={1} key={index}>
-                      <TextField
-                        //   {...formMethods.register(`options[${index}].text`)}
-                        fullWidth
-                        placeholder={`Option ${index + 1}`}
-                        defaultValue={option.text}
-                      />
-                      <IconButton>
-                        <CloseIcon />
-                      </IconButton>
-                    </Box>
-                  ))}
-                <Button
-                  startIcon={<AddIcon />}
-                  variant="outlined"
-                  fullWidth
-                  sx={{ mt: 2 }}
-                  //   onClick={() =>
-                  //     formMethods.setValue("options", [
-                  //       ...formMethods.getValues("options"),
-                  //       { text: "" },
-                  //     ])
-                  //   }
-                >
-                  <Trans i18nKey="newOption" />
-                </Button>
-              </Grid>
-            </Grid>
-
-            {/* Section 2: Options Impacts */}
-            <Typography variant="subtitle1" gutterBottom sx={{ mt: 3 }}>
-              <Trans i18nKey="optionsImpacts" />
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              Impact indicates how options affect various attributes.
-            </Typography>
-            <Button variant="outlined" fullWidth sx={{ mt: 2 }} disabled>
-              <Trans
-                i18nKey="selectAttribute"
-                disabled={fetchAttributeKit?.data?.items?.length === 0}
-              />
+                <IconButton>
+                  <CloseIcon />
+                </IconButton>
+              </Box>
+            ))} */}
+            <Button
+              startIcon={<AddIcon />}
+              variant="outlined"
+              sx={{ mt: 2 }}
+              size="small"
+            >
+              <Trans i18nKey="newOption" />
             </Button>
+          </Grid>
+        </Grid>
 
-            {/* Section 3: Advanced Settings */}
-            <Typography variant="subtitle1" gutterBottom sx={{ mt: 3 }}>
-              <Trans i18nKey="advancedSettings" />
-            </Typography>
-            <Typography variant="body2" color="textSecondary">
-              In this section, you can make ancillary settings for the question.
-            </Typography>
-            <Grid container spacing={2} alignItems="center">
-              <Grid item xs={6}>
-                <Typography variant="body2">
-                  <Trans i18nKey="notApplicable" />
-                </Typography>
-                <Switch
-                  {...formMethods.register("mayNotBeApplicable")}
-                  checked={formMethods.watch("mayNotBeApplicable")}
-                />
-              </Grid>
-              <Grid item xs={6}>
-                <Typography variant="body2">
-                  <Trans i18nKey="notAdvisable" />
-                </Typography>
-                <Switch
-                  {...formMethods.register("advisable")}
-                  checked={formMethods.watch("advisable")}
-                />
-              </Grid>
-            </Grid>
-            <Box display="flex" justifyContent="space-between" p={2}>
-              <Button onClick={onClose}>
-                <Trans i18nKey="cancel" />
-              </Button>
-              <Button type="submit" variant="contained" color="primary">
-                <Trans i18nKey="createQuestion" />
-              </Button>
+        <Typography variant="subtitle1" gutterBottom sx={{ mt: 3 }}>
+          <Trans i18nKey="optionsImpacts" />
+        </Typography>
+        {fetchAttributeKit?.data?.items?.length === 0 && (
+          <EmptyState
+            btnTitle={"newOptionImpact"}
+            title={"optionsImpactsEmptyState"}
+            SubTitle={"optionsImpactsEmptyStateDetailed"}
+            onAddNewRow={handleAddNewRow}
+            disabled={fetchAttributeKit?.data?.items?.length === 0}
+          />
+        )}
+
+        <Box display="flex" flexDirection="column" gap={1}>
+          <Typography variant="semiBoldXLarge" gutterBottom>
+            <Trans i18nKey="advancedSettings" />
+          </Typography>
+          <Typography variant="bodyMedium" color="textSecondary">
+            <Trans i18nKey="advancedSettingsDescription" />
+          </Typography>
+        </Box>
+        <Grid container spacing={2} alignItems="center" mt={1}>
+          <Grid item xs={6}>
+            <Box sx={{ ...styles.centerVH }}>
+              <Typography variant="semiBoldMedium">
+                <Trans i18nKey="notApplicable" />
+              </Typography>
+              <Switch
+                {...formMethods.register("mayNotBeApplicable")}
+                checked={formMethods.watch("mayNotBeApplicable")}
+              />
             </Box>
-          </form>
-        </FormProvider>
-      </DialogContent>
-    </Dialog>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="semiBoldMedium">
+              <Trans i18nKey="notAdvisable" />
+            </Typography>
+            <Switch
+              {...formMethods.register("advisable")}
+              checked={formMethods.watch("advisable")}
+            />
+          </Grid>
+        </Grid>
+      </FormProviderWithForm>
+
+      <CEDialogActions
+        loading={false}
+        onClose={onClose}
+        onSubmit={formMethods.handleSubmit(onSubmit)}
+        submitButtonLabel="createQuestion"
+        type="create"
+      />
+    </CEDialog>
   );
 };
 
